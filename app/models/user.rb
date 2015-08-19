e28ad9e2b5a3bf971cf698567bbb4943425939e0
Steven Chen <sccchenster@gmail.com>
2015-08-18 23:29:18 -0700

    Fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token)
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)

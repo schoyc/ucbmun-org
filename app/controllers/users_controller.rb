8709c75dec090de5ed982a14ca2769f8d7b75c9c
Steven Chen <sccchenster@gmail.com>
2015-09-30 19:13:30 -0700

    Took out update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,19 +30,6 @@ class UsersController < ApplicationController
   def registrationerror
   end
 
-  def update
-  @user = User.find(params[:id])
-  if params[:user][:password].blank?
-    @user.update_without_password(params[:user])
-  else
-    @user.update_attributes(params[:user])
-  end
-  if @user.errors.blank?
-    redirect_to admin_users_path, :notice => "User updated successfully."
-  else
-    render :edit
-  end
-
 
   def index
     @registrations = User.all

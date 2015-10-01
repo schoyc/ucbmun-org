730c04c383d96d8b0150c6dc10c89b79f93aab77
Steven Chen <sccchenster@gmail.com>
2015-09-30 19:02:40 -0700

    Fixed wikileaks

--- db/migrate/20150916050846_devise_create_admin_users.rb
+++ db/migrate/20150916050846_devise_create_admin_users.rb
@@ -2,8 +2,8 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def change
     create_table(:admin_users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: "scchen@ucbmun.com"
-      t.string :encrypted_password, null: false, default: "password()#"
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token

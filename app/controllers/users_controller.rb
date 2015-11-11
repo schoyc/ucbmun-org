5314b99f189244ead7e7b2a384c630bda1a579aa
Steven Chen <sccchenster@gmail.com>
2015-11-10 22:13:09 -0800

    Added edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -23,11 +26,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def balance
     @user = User.find(params[:id])
   end
 
   def registrationerror
+  end
+
+  def delegation
+    @user = User.find(params[:id])
   end
 
 
@@ -43,6 +65,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :university, :password, :password_confirmation,
                                     :username, :phone, :address, :city, :state, :zip, :country, :delegates_count)
+    end
+
+        # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 
 end

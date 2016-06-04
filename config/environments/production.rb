6d4be3c89b09a31679f5df0ddbc368d245eb6992
Steven Chen <sccchenster@gmail.com>
2016-06-03 23:14:09 -0700

    Remove email password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
               :address              => "smtp.gmail.com",
               :port                 => 587,
               :domain               => "gmail.com",
-              :user_name            => 'ucbmun.register@gmail.com',
-              :password             => 'ucbmunXX',
+              :user_name            => ENV['REGISTER_EMAIL'],
+              :password             => ENV['REGISTER_PASSWORD'],
               :authentication       => 'plain',
               :enable_starttls_auto => true  }
 
